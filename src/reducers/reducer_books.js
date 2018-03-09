51cd1b66dd0e384466fa818cd4b342948aeee7f3
Peter Higgins <pah@cenabo.com>
2018-03-09 12:28:28 +0000

    Add books and activeBooks reducers, as well as associated actions.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Book 1', pages: 101 },
+    { title: 'Book 2', pages: 201 },
+    { title: 'Book 3', pages: 301 },
+    { title: 'Book 4', pages: 401 }
+  ];
+}
